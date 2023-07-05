77d0ee37228e6aace9db3bdc2d588569af452b8d
Illia <Illia@DESKTOP-TG4JUT1>
2023-07-05 16:02:51 +0200

    +add DB

---
+++ ProjPUBG/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace ProjPUBG
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<ContentAdmin> ContentAdmins { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
