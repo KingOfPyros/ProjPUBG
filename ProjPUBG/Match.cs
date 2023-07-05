6a5869de9dbae612e97840ad4db1750a3598cf4b
Illia <Illia@DESKTOP-TG4JUT1>
2023-07-05 15:24:50 +0200

    +add models

---
+++ ProjPUBG/Match.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjPUBG
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ ProjPUBG/Weapon.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjPUBG
+{
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Damage { get; set; }
+    }
+}
