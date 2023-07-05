17b155455f978cb350e29e41dd2ff2acd7279d74
Illia <Illia@DESKTOP-TG4JUT1>
2023-07-05 15:26:32 +0200

    +add admin

---
+++ ProjPUBG/ContentAdmin.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjPUBG
+{
+    public class ContentAdmin
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
