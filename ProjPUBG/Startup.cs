38b4ef72087f65439a96f776569f26df9a594299
Illia <Illia@DESKTOP-TG4JUT1>
2023-07-05 16:42:49 +0200

    +add startup

---
+++ ProjPUBG/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjPUBG
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services, WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("Data Source=SQL6030.site4now.net;Initial Catalog=db_a9b162_illiakursnew2;User Id=db_a9b162_illiakursnew2_admin;Password=Qwerty123");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddControllers();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
